cae30bb5db1dddb95f569eeb9f9fa31bc6030ae8
catduckgnaf <github@catduck.org>
2023-11-27 11:15:36 -0500

    Update config_flow.py

--- custom_components/rtl_433/config_flow.py
+++ custom_components/rtl_433/config_flow.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 import voluptuous as vol
-
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_PORT
 from homeassistant.helpers import selector
