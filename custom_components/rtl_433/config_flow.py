c204a79b369e0a8d43b145710148502406d1c926
catduckgnaf <github@catduck.org>
2023-11-27 11:48:51 -0500

    Update config_flow.py

--- custom_components/rtl_433/config_flow.py
+++ custom_components/rtl_433/config_flow.py
@@ -1,6 +1,8 @@
 """Adds config flow for rtl_433."""
 from __future__ import annotations
 
+import asyncio
+import subprocess
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_PORT
@@ -13,6 +15,19 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
+    async def _test_connection(self, host: str, port: int) -> bool:
+        """Test the connection to the specified host and port."""
+        try:
+            result = await asyncio.to_thread(subprocess.run, 
+                ["ping", "-c", "1", f"{host}:{port}"],  # Adjust for Windows if needed
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            return result.returncode == 0
+        except Exception as e:
+            LOGGER.error(f"Error testing connection: {e}")
+            return False
+
     async def async_step_user(
         self,
         user_input: dict | None = None,
@@ -20,24 +35,33 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle a flow initialized by the user."""
         _errors = {}
         if user_input is not None:
+            host = user_input[CONF_HOST]
+            port = user_input[CONF_PORT]
+
+            # Test the connection before proceeding
             try:
-                # Replace username with host and password with port
-                await self._test_credentials(
-                    host=user_input[CONF_HOST],
-                    port=user_input[CONF_PORT],
-                )
-            except IntegrationBlueprintApiClientAuthenticationError as exception:
-                LOGGER.warning(exception)
-                _errors["base"] = "auth"
-            except IntegrationBlueprintApiClientCommunicationError as exception:
-                LOGGER.error(exception)
-                _errors["base"] = "connection"
-            except IntegrationBlueprintApiClientError as exception:
-                LOGGER.exception(exception)
+                if not await self._test_connection(host, port):
+                    _errors["base"] = "cannot_connect"
+            except Exception as e:
+                LOGGER.error(f"Error testing connection: {e}")
                 _errors["base"] = "unknown"
-            else:
+
+            if not _errors:
+                # Continue with additional credential testing if needed
+                # try:
+                #     await self._test_credentials(host=host, port=port)
+                # except IntegrationBlueprintApiClientAuthenticationError as exception:
+                #     LOGGER.warning(exception)
+                #     _errors["base"] = "auth"
+                # except IntegrationBlueprintApiClientCommunicationError as exception:
+                #     LOGGER.error(exception)
+                #     _errors["base"] = "connection"
+                # except IntegrationBlueprintApiClientError as exception:
+                #     LOGGER.exception(exception)
+                #     _errors["base"] = "unknown"
+
                 return self.async_create_entry(
-                    title=user_input[CONF_HOST],
+                    title=host,
                     data=user_input,
                 )
 
@@ -63,4 +87,3 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             ),
             errors=_errors,
         )
-
