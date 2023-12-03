81778d834ebbd9eb7ed445fe4fad8b4d7b979bea
catduckgnaf <github@catduck.org>
2023-12-02 19:43:09 -0500

    Update rtl_433.py

--- custom_components/rtl_433/rtl_433.py
+++ custom_components/rtl_433/rtl_433.py
@@ -4,9 +4,8 @@ import logging
 import random
 import re
 import aiohttp
-import websocket
+import websockets
 from json.decoder import JSONDecodeError
-import websocket
 
 from .const import (CONFIG_CMD, DEFAULT_TIME, DISMISS_ALERT_CMD, START_CMD,
                     STATUS_CMD, STOP_CMD)
