6ff80fe04319759e602a061bbc4a53a510d0d8b1
catduckgnaf <github@catduck.org>
2023-12-03 10:32:43 -0500

    Update config_flow.py

--- custom_components/rtl_433/config_flow.py
+++ custom_components/rtl_433/config_flow.py
@@ -15,27 +15,28 @@ class RtlFlowHandler(config_entries.ConfigFlow):
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
-        """Handle a flow start."""
-        _LOGGER.debug(f"Starting async_step_user of {DEFAULT_NAME}")
+async def async_step_user(self, user_input=None):
+    """Handle a flow start."""
+    _LOGGER.debug(f"Starting async_step_user of {DEFAULT_NAME}")
 
-        errors = None
+    errors = None
 
-        if user_input is not None:
-            # Filter out any extra keys from user_input
-            filtered_user_input = {
-                key: user_input[key] for key in ['WS_IP', 'WS_PORT']
-            }
+    if user_input is not None:
+        # Filter out any extra keys from user_input
+        expected_keys = ['WS_IP', 'WS_PORT']
+        filtered_user_input = {key: user_input[key] for key in expected_keys if key in user_input}
 
-            await self.async_set_unique_id(secrets.token_hex(8))
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(title=DEFAULT_NAME, data=filtered_user_input)
+        await self.async_set_unique_id(secrets.token_hex(8))
+        self._abort_if_unique_id_configured()
+        return self.async_create_entry(title=DEFAULT_NAME, data=filtered_user_input)
 
-        new_user_input = {
-            vol.Required(WS_IP, description="Enter the IP address of the RTL_433 webserver", default=WS_IP): str,
-            vol.Required(WS_PORT, description="Enter the port number of the RTL_433 webserver", default=WS_PORT): int,
-        }
+    new_user_input = {
+        vol.Required(WS_IP, description="Enter the IP address of the RTL_433 webserver", default=WS_IP): str,
+        vol.Required(WS_PORT, description="Enter the port number of the RTL_433 webserver", default=WS_PORT): int,
+    }
 
-        schema = vol.Schema(new_user_input)
+    schema = vol.Schema(new_user_input)
+
+    return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
